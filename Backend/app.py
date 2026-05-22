0e21d9aedcac561dc1aa21ba9a839c7b1f625cd2
tylernsocial <tylernsocial@gmail.com>
2026-05-21 21:46:11 -0600

    Update comments in app.py and notes.txt for clarity on backend functionality and framework usage

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-## goal of app.py:
+## goal of app.py: essentially will act as the backend server 
 # overview:
 # react frontend
 # User uploads image
@@ -11,3 +11,10 @@
         #↓
 # React frontend displays result
 
+# will need to routes, get and post route, GET to check if the 
+# backend is running and POST to get the prediction but it accepts 
+# and image file which means we are sending data to the backend 
+
+# will also need the import CORS since react will use a different port than Flask,
+# and by default browsers may block some requests between different origins for security reasons
+# CORS(app) allows for the React frontend to call this backend
